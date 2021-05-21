682f50db864585a6954fe7c43136b7947c530167
Shailendra Malviya <78775574+ShailendraMalviya@users.noreply.github.com>
2021-05-21 12:02:24 +0530

    Second File

---
+++ Access12.java
@@ -0,0 +1,17 @@
+class A
+{
+	private int i=40;
+	public void menu(){
+		System.out.println("Hello Dodg");
+		}
+}
+
+public class Access12{
+	public static void main(String arr[]){
+		A obj = new A();
+		//System.out.println(obj.i);
+		obj.menu();
+		}
+}
+
+	
